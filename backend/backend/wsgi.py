9e0d714f11709ee2ac05d8dbe7cb43a574f67b61
Jacob <brewbakerjacob@gmail.com>
2025-10-23 18:11:11 -0400

    comment out apps 9

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -5,6 +5,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+
+
+bruh
 """
 
 import os
