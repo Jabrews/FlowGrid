df275f9f3031ad31c03ec1d39bcddd5b6171b89e
Jacob <brewbakerjacob@gmail.com>
2025-11-11 20:38:04 -0500

    fixing secrets 3

--- frontend/src/components/hooks/useCsrf.tsx
+++ frontend/src/components/hooks/useCsrf.tsx
@@ -3,7 +3,7 @@ import { useCookies } from "react-cookie"
 // custom hook for getting csrf cookie
 export default function useCsrf() {
 
-    const [cookies, ] = useCookies(["csrftoken"]);
+    const [cookies] = useCookies(["csrftoken"]);
     return cookies.csrftoken
 
 }
